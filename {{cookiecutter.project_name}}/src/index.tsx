713793b8fe6fc470d483e62ca2b0569da989d186
foodproduct <dcbarnez@gmail.com>
2023-03-14 10:26:01 -0400

    minor fixes

--- {{cookiecutter.project_name}}/src/index.tsx
+++ {{cookiecutter.project_name}}/src/index.tsx
@@ -4,6 +4,7 @@ import App from "./components/App";
 
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("cortex-plugin-root");
-  const root = createRoot(container);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const root = createRoot(container!);
   root.render(<App />);
 });
